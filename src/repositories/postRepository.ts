7835f76a23945f61c17bb4a6a7cd52e856f06edb
Misael Perez <misaelperezoficial@gmail.com>
2024-11-08 23:02:53 -0500

    refactor: refactor Post repository for new database connection structure
    Updated the Post repository to support the refactored database connection class, enabling dependency
    injection for improved modularity.

--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -1,14 +1,14 @@
-import { dbConnection } from "database/dbConnection";
+import { Database } from "models/dataBase";
 import { Repository } from "models/baseRepositoryModel";
 import { Post } from "models/postModel";
 
 export class PostRepository implements Repository<Post> {
   private selectAllPost = "SELECT * FROM post";
 
-  async getAll(): Promise<Post[]> {
-    const [posts] = await dbConnection.query<Post[]>(this.selectAllPost);
-    await dbConnection.end();
+  constructor(private dbConnection: Database) {}
 
+  async getAll(): Promise<Post[]> {
+    const posts = await this.dbConnection.queryRows<Post>(this.selectAllPost);
     return posts;
   }
 }
